ac13ed5d8169955f6409ca874f8b39bb759ab254
dongdong <1252603486@qq.com>
2024-09-04 14:35:44 +0800

    feat(全局异常处理): 增加全局异常处理handler

---
+++ src/main/java/com/github/forestworld/forestworldblog/exception/BusinessException.java
@@ -0,0 +1,44 @@
+package com.github.forestworld.forestworldblog.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+public  class BusinessException extends RuntimeException{
+    /**
+     * Error errorData.
+     */
+    private Object errorData;
+
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Http status code
+     *
+     * @return {@link HttpStatus}
+     */
+
+    @Nullable
+    public Object getErrorData() {
+        return errorData;
+    }
+
+    /**
+     * Sets error errorData.
+     *
+     * @param errorData error data
+     * @return current exception.
+     */
+    @NonNull
+    public BusinessException setErrorData(@Nullable Object errorData) {
+        this.errorData = errorData;
+        return this;
+    }
+}
