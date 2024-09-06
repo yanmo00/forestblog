f7e87d0241f8e747aedc51bf5e76bb5c8031995a
money <129597196+yanmo00@users.noreply.github.com>
2024-09-06 11:29:07 +0800

    feat article table init.sql and DDL.sql

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -20,3 +20,22 @@ COMMENT ON COLUMN "users".username IS '用户名';
 COMMENT ON COLUMN "users".password IS '密码';
 COMMENT ON COLUMN "users".email IS '邮箱';
 COMMENT ON COLUMN "users".avatar IS '头像';
+
+create table "articles"
+(
+    id          bigserial
+            primary key,
+    title       varchar       not null,
+    content     varchar       not null,
+    author      varchar       not null,
+    publishdate timestamp     not null
+);
+
+alter table "articles"
+    owner to beiyouge;
+COMMENT ON TABLE "articles" IS '文章表';
+COMMENT ON COLUMN "articles".title IS '标题';
+COMMENT ON COLUMN "articles".content IS '内容';
+COMMENT ON COLUMN "articles".author IS '作者';
+COMMENT ON COLUMN "articles".publishdate IS '发布日期';
+
